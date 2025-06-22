12636c3991e8dfb8df85d8379dbca03b8e8423e7
yasu-85tk <85tk.zip2@gmail.com>
2025-06-22 20:37:51 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball44/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/rules.mk
@@ -30,14 +30,14 @@ MOUSEKEY_ENABLE = no
 
 # Enabled only one of RGBLIGHT and RGB_MATRIX if necessary.
 RGBLIGHT_ENABLE = no        # Enable RGBLIGHT
-RGB_MATRIX_ENABLE = yes      # Enable RGB_MATRIX (not work yet)
+RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not work yet)
 RGB_MATRIX_DRIVER = ws2812
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 # To support OLED
-OLED_ENABLE = yes                # Please Enable this in each keymaps.
+OLED_ENABLE = no                # Please Enable this in each keymaps.
 SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
 
 # Include common library
