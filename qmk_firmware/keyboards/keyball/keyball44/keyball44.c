2c11043ed7014306a48444ab664f85f1982e5995
yasu-85tk <85tk.zip2@gmail.com>
2025-06-24 23:03:45 +0900

    Update keyball44.c

--- qmk_firmware/keyboards/keyball/keyball44/keyball44.c
+++ qmk_firmware/keyboards/keyball/keyball44/keyball44.c
@@ -44,3 +44,42 @@ void keyball_on_adjust_layout(keyball_adjust_t v) {
     rgblight_set_effect_range(0, lednum_this + lednum_that);
 #endif
 }
+
+#include "keyball44.h"
+
+// LED Configuration
+led_config_t g_led_config = { {
+  // LED Index mapping (行列 → LED)
+  {     18,   15,     11,     7,     4,    1 },
+  {     19,   16,     12,     8,     5,    2 },
+  {     20,   17,     13,     9,     6,    3 },
+  { NO_LED,   14,     10,     28,   29,   30 },
+  {     41,   44,     48,     51,   54,   57 },
+  {     42,   45,     49,     52,   55,   58 },
+  {     43,   46,     50,     53,   56,   59 },
+  { NO_LED,   47, NO_LED, NO_LED,   32,   31 }
+}, {
+  // LED Index → 座標 (0〜255の範囲)
+  {85,16}, {85,32}, {68,0}, {68,21}, {68,42}, {51,0}, {51,21}, {51,42}, {51,63}, {34,0},
+  {34,21}, {34,42}, {34,63}, {17,0}, {17,21}, {17,42}, {17,42}, {0,21}, {0,42}, {0,42},
+  {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {85,63}, {102,63}, {119,63}, {170,63},
+  {187,63}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {255,21}, {255,42},
+  {238,0}, {238,21}, {238,42}, {238,63}, {221,0}, {221,21}, {221,42}, {204,0}, {204,21},
+  {204,42}, {187,0}, {187,21}, {187,42}, {170,0}, {170,21}, {170,42}, {170,42}, {0,0}
+}, {
+  // LED フラグ（キー点灯 or 無効）
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE,
+  LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE,
+  LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE, LED_FLAG_NONE,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+  LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_NONE
+} };
