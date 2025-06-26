e65f61840e029cd1a60421f8cb3aee1b247f5879
yasu-85tk <85tk.zip2@gmail.com>
2025-06-26 19:22:05 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/config.h
@@ -50,8 +50,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_TYPING_HEATMAP_AREA_LIMIT 16
 #define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 25
 
-#define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#define ENABLE_RGB_MATRIX_RAINDROPS
 
 #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+
 
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP// Sets the default mode, if none has been set
